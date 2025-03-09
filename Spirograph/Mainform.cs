6fd70a210490108d4a13c71eb3bfaf3f1cc90c76
resoftw <robyedrian@gmail.com>
2025-03-09 23:09:16 +0700

    ,

--- Spirograph/Mainform.cs
+++ Spirograph/Mainform.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing.Drawing2D;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace Grafik
 {
@@ -6,26 +8,29 @@ namespace Grafik
     {
 
         private System.Windows.Forms.Timer timer;
-        private BufferedGraphicsContext context;
-        private BufferedGraphics buffer;
 
-        private Spiro spiro;
-
+        private ASpiro spiro;
+        private Bitmap bitmap;
+        private Graphics bufferGraphics;
+        private int frmnum = 0;
         public FMain()
         {
             this.ClientSize = new Size(540, 960);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.DoubleBuffered = true;
 
-            context = BufferedGraphicsManager.Current;
-            buffer = context.Allocate(this.CreateGraphics(), this.ClientRectangle);
-            spiro= new Spiro(this);
+            bitmap = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            bufferGraphics = Graphics.FromImage(bitmap);
+            spiro = new(this);
+            System.IO.Directory.CreateDirectory("./Output");
 
             timer = new System.Windows.Forms.Timer();
             timer.Interval = 1; 
             timer.Tick += (s, e) =>
             {
                 this.Invalidate();
+                //bitmap.Save("./Output/Spiro_" + frmnum.ToString("00000") + ".jpg",ImageFormat.Jpeg);
+                frmnum++;
             };
 
             timer.Start();
@@ -34,17 +39,15 @@ namespace Grafik
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            DrawGrafik(buffer.Graphics);
-            buffer.Render(e.Graphics);
+            DrawGrafik(bufferGraphics);
+            e.Graphics.DrawImage(bitmap, 0, 0);
         }
  
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
-            if (context != null)
-            {
-                buffer = context.Allocate(this.CreateGraphics(), this.ClientRectangle);
-            }
+            bitmap = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            bufferGraphics = Graphics.FromImage(bitmap);
         }
         private void DrawGrafik(Graphics g)
         {
